0f1a3a5bfaee10a4480294f70173dc81782d43fb
Facu <facundojsole@gmail.com>
2024-04-01 12:13:24 -0300

    add new margin and padding stylings for desktop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import Chevron from "./components/icons/Chevron";
 
 export default function Home() {
   return (
-    <div className="p-6">
+    <div className="p-6 md:px-16 md:py-6">
       <Navbar />
       <Hero />
-      <div className="grid place-items-center">
+      <div className="grid place-items-center mt-8 md:mt-24">
         <Chevron />
       </div>
     </div>
