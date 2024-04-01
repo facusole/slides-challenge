9a273a3fbdab7c04ccf9761b7e424d565fdb6afc
Facu <facundojsole@gmail.com>
2024-04-01 16:24:54 -0300

    add new conditional classes

--- src/app/components/icons/HamburgerIcon.tsx
+++ src/app/components/icons/HamburgerIcon.tsx
@@ -1,4 +1,8 @@
-export default function HamburgerIcon() {
+type HamburgeIconProps = {
+  isActive: boolean;
+};
+
+export default function HamburgerIcon({ isActive }: HamburgeIconProps) {
   return (
     <svg
       width="40"
@@ -6,10 +10,10 @@ export default function HamburgerIcon() {
       viewBox="0 0 40 40"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      className={`relative z-10 ${isActive ? "stroke-white" : "stroke-black"}`}
     >
       <path
         d="M13.5 10C23.6536 10 33 10 33 10M7 19.75H33M13.5 29.5H33"
-        stroke="black"
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
