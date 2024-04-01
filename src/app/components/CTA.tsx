3bb7c1625e92e490055c3a70ee52cea4b32a7d7a
Facu <facundojsole@gmail.com>
2024-04-01 12:13:48 -0300

    add desktop stylings and hover effects

--- src/app/components/CTA.tsx
+++ src/app/components/CTA.tsx
@@ -6,7 +6,7 @@ const lato = Lato({ weight: ["700"], subsets: ["latin"] });
 export default function CTA() {
   return (
     <button
-      className={`${lato.className} flex w-full items-center justify-center gap-2 rounded-lg py-5 bg-black text-white`}
+      className={`${lato.className} flex w-full items-center justify-center gap-2 rounded-lg py-5 bg-black text-white md:w-fit md:px-14 hover:bg-[#1D1D1D] transition-colors select-none`}
     >
       Get started <Arrow />
     </button>
