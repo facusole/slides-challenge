db0bbf3685650221e66a64c07b9003a5f0139a7f
Facu <facundojsole@gmail.com>
2024-04-01 11:17:49 -0300

    add new cta component

---
+++ src/app/components/CTA.tsx
@@ -0,0 +1,9 @@
+import Arrow from "./icons/Arrow";
+
+export default function CTA() {
+  return (
+    <button className="flex w-full items-center justify-center gap-2 rounded-lg py-5 bg-black text-white">
+      Get started <Arrow />
+    </button>
+  );
+}
