21aed4a5d6cacbee4d44e8b114a9f04db7f6a09d
Facu <facundojsole@gmail.com>
2024-04-01 16:24:42 -0300

    add new components to the navbar, add new states, add new animations

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,11 +1,23 @@
+"use client";
+
+import { useState } from "react";
 import HamburgerIcon from "./icons/HamburgerIcon";
+import Menu from "./Menu";
+import { AnimatePresence } from "framer-motion";
 
 export default function Navbar() {
+  const [isActive, setIsActive] = useState(false);
+
   return (
     <nav className="flex justify-end mb-6">
-      <button>
-        <HamburgerIcon />
+      <button
+        onClick={() => {
+          setIsActive(!isActive);
+        }}
+      >
+        <HamburgerIcon isActive={isActive} />
       </button>
+      <AnimatePresence mode="wait">{isActive && <Menu />}</AnimatePresence>
     </nav>
   );
 }
