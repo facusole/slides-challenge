96ef4f346e2b78cd6a073bd753db7688952c5e71
Facu <facundojsole@gmail.com>
2024-04-01 11:26:22 -0300

    change font to cta component

--- src/app/components/CTA.tsx
+++ src/app/components/CTA.tsx
@@ -1,8 +1,13 @@
 import Arrow from "./icons/Arrow";
+import { Lato } from "next/font/google";
+
+const lato = Lato({ weight: ["700"], subsets: ["latin"] });
 
 export default function CTA() {
   return (
-    <button className="flex w-full items-center justify-center gap-2 rounded-lg py-5 bg-black text-white">
+    <button
+      className={`${lato.className} flex w-full items-center justify-center gap-2 rounded-lg py-5 bg-black text-white`}
+    >
       Get started <Arrow />
     </button>
   );
