1f35eedd1baa1a6dd04ff6ab27e4e17be9898d66
Facu <facundojsole@gmail.com>
2024-04-01 12:14:24 -0300

    add new margin for spacing

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,8 +2,10 @@ import HamburgerIcon from "./icons/HamburgerIcon";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-end">
-      <HamburgerIcon />
+    <nav className="flex justify-end mb-6">
+      <button>
+        <HamburgerIcon />
+      </button>
     </nav>
   );
 }
